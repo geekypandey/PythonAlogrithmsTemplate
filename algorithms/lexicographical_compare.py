26cf35da94322ec1dddb9b570b8c7fd4d571cfb7
geekypandey <anuragcarbon1998@gmail.com>
2020-11-20 09:59:35 +0530

    refactored lexicographical compare in algorithms

--- algorithms/lexicographical_compare.py
+++ algorithms/lexicographical_compare.py
@@ -1,9 +1,9 @@
+import itertools
+
 def lexicographical_compare(first, second):
-    for e, f in zip(first, second):
-        if f < e:
-            return False
-        elif e < f:
+    for e, f in itertools.zip_longest(first, second):
+        if e is None or (f is not None and e < f):
             return True
-    if len(second) <= len(first):
-        return False
-    return True
+        elif f is None or f < e:
+            return False
+    return False
